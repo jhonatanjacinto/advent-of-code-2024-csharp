6a876776b24a638d4a7f6564db114672818667e5
Jhonatan Jacinto <jhonatanjacinto@gmail.com>
2024-12-11 16:30:06 -0300

    Part Two - Day Three

--- Day3/Program.cs
+++ Day3/Program.cs
@@ -5,17 +5,21 @@ var watcher = new Stopwatch();
 watcher.Start();
 var filePath = Path.Combine(Directory.GetCurrentDirectory(), "inputDay3.txt");
 var fileContent = File.ReadAllText(filePath);
-var regex = @"(mul\([0-9]+\,[0-9]+\))";
-var matches = Regex.Matches(fileContent, regex);
+var regex = new Regex(@"(don't\(\)).*?(?=do|don't\(\)|\Z)", RegexOptions.Singleline);
+var regexMul = @"(mul\([0-9]+\,[0-9]+\))";
+fileContent = regex.Replace(fileContent, "");
+var matches = Regex.Matches(fileContent, regexMul);
 var total = 0;
 
 foreach (Match match in matches)
 {
     var mulOperators = Regex.Replace(match.Value, @"(mul\(|\))", "");
-    var operators = Array.ConvertAll(mulOperators.Split(","), int.Parse);
+    var operators = Array.ConvertAll(mulOperators.Split(",", StringSplitOptions.RemoveEmptyEntries), int.Parse);
     total += operators[0] * operators[1];
 }
 
 Console.WriteLine($"Total: {total}");
 watcher.Stop();
 Console.WriteLine($"Execution Time: {watcher.ElapsedMilliseconds} ms");
+
+Console.ReadKey();
