1665268548b4523678ffe9ef06828f31a9f7c504
Jhonatan Jacinto <jhonatanjacinto@gmail.com>
2024-12-11 02:44:49 -0300

    Day Two - Part 2

--- DayTwo/Program.cs
+++ DayTwo/Program.cs
@@ -1,36 +1,40 @@
 ﻿var filePath = Path.Combine(Directory.GetCurrentDirectory(), "inputDay2.txt");
-var safeReports = 0;
 
 var reports = File.ReadLines(filePath)
         .Select(line => line.Split(' ', StringSplitOptions.RemoveEmptyEntries)
         .Select(int.Parse).ToArray()).ToList();
 
-foreach (var report in reports)
-{
-    var lastDirection = ReportDirection.Unknown;
-    var isSafe = true;
-    
-    for (var i = 0; i < report.Length - 1; i++)
-    {
-        var differFactor = report[i] - report[i + 1];
-        var currentDirection = differFactor < 0 ? ReportDirection.Decreasing : ReportDirection.Increasing;
-        if (lastDirection == ReportDirection.Unknown) lastDirection = currentDirection;
-        if (currentDirection != lastDirection || Math.Abs(differFactor) > 3 || differFactor == 0)
-        {
-            isSafe = false;
-            break;
-        }
-    }
-
-    if (isSafe)
-    {
-        safeReports++;
-    }
-}
+var safeReports = reports.Count(report => IsReportSafe(report));
 
 Console.WriteLine($"Safe Reports: {safeReports}");
 
 Console.ReadKey();
+return;
+
+static bool IsReportSafe(int[] report, bool shouldCheckFurther = true)
+{
+    var lastDirection = ReportDirection.Unknown;
+    
+    for (var i = 0; i < report.Length - 1; i++)
+    {
+        var differFactor = report[i] - report[i + 1];
+        var currentDirection = differFactor < 0 ? ReportDirection.Increasing : ReportDirection.Decreasing;
+        if (lastDirection == ReportDirection.Unknown) lastDirection = currentDirection;
+        if (currentDirection != lastDirection || Math.Abs(differFactor) > 3 || differFactor == 0)
+        {
+            if (!shouldCheckFurther) return false;
+
+            var report0 = report.Where((n, index) => index != 0).ToArray();
+            var report1 = report.Where((n, index) => index != i).ToArray();
+            var report2 = report.Where((n, index) => index != i + 1).ToArray();
+            var isSafe = IsReportSafe(report0, false) || IsReportSafe(report1, false) || IsReportSafe(report2, false);
+            
+            return isSafe;
+        }
+    }
+    
+    return true;
+}
 
 internal enum ReportDirection
 {
